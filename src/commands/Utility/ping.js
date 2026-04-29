22fd8439c85c98965b5e14f75bcfc46834d44a56
Dieguz <diegotrombini26@gmail.com>
2026-04-29 07:17:47 +0200

    Update ping.js

--- src/commands/Utility/ping.js
+++ src/commands/Utility/ping.js
@@ -1,17 +1,15 @@
 import { SlashCommandBuilder } from 'discord.js';
 
 export default {
-    // 1. Definiamo i dati del comando (quello che vedi su Discord)
+    category: "utility",
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Controlla se il bot è online e la sua velocità'),
 
-    // 2. Definiamo cosa succede quando il comando viene eseguito
     async execute(interaction, config, client) {
-        // interaction.reply è il modo in cui il bot risponde al comando /
-        await interaction.reply({ 
+        await interaction.reply({
             content: `🏓 Pong! La latenza del bot è di **${client.ws.ping}ms**.`,
-            ephemeral: true // Solo tu potrai vedere questo messaggio (opzionale)
+            ephemeral: true
         });
     },
 };
