bd61d47eefce86323ccf12e67e00249152899921
Dieguz <diegotrombini26@gmail.com>
2026-04-30 15:20:06 +0200

    Update fattura.js

--- src/commands/Utility/fattura.js
+++ src/commands/Utility/fattura.js
@@ -1,14 +1,3 @@
-async execute(interaction, config, client) {
-    // SOSTITUISCI '123456789012345678' con l'ID reale del tuo ruolo
-    const roleId = '1498385283186429972'; 
-
-    if (!interaction.member.roles.cache.has(roleId)) {
-        return await interaction.reply({ 
-            content: "❌ Non hai il permesso di emettere una fattura!", 
-            ephemeral: true 
-        });
-    }
-}
 import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
 
 export default {
