1960464de5f7a612b4aa9b8df1a751403135efe4
Dieguz <diegotrombini26@gmail.com>
2026-04-30 10:31:07 +0200

    Update ping.js

--- src/commands/Utility/ping.js
+++ src/commands/Utility/ping.js
@@ -2,11 +2,12 @@ import { SlashCommandBuilder } from 'discord.js';
 
 export default {
     category: "utility",
+    // Questo blocco sostituisce @commands.hybrid_command
     data: new SlashCommandBuilder()
-        .setName('ping') 
-        .setDescription('Mostra la velocità e la latenza del bot'),
+        .setName('ping') // Il "name" dello script Python
+        .setDescription('🏓Pong, mostra la velocità e la latenza del bot'), // La "description"
 
-    async execute(interaction, config, client) {
+async execute(interaction, config, client) {
         // Calcoliamo la latenza del WebSocket (la velocità di risposta di Discord)
         const ping = client.ws.ping;
         
